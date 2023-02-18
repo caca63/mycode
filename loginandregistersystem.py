8e62d4bfd6bc14266a0efee8173b587896fe2a12
firefood <65716578+caca63@users.noreply.github.com>
2023-02-18 14:37:17 +0100

    V2.0 FINAL VERSION
    Text "plot" system added.

--- loginandregistersystem.py
+++ loginandregistersystem.py
@@ -1,7 +1,7 @@
 userdata = ["", ""]
 
 
-def login(username_data, passwords_data, username_):
+def login(username_data, passwords_data, username_, t):
     username_found = False
     username_idx = 0
     for l in username_data:
@@ -12,23 +12,28 @@ def login(username_data, passwords_data, username_):
             username_found = True
         username_idx += 1
     if not username_found:
-        username_ = input("This username doesn't exist, please try again : ")
-        login(username_data, passwords_data, username_)
+        print("This username doesn't exist, please try again : ")
+        username_ = input("-> ")
+        login(username_data, passwords_data, username_, t)
 
     userdata[0] = username_
 
     password = passwords_data[username_idx - 1]
     print("---------Login---------")
-    password_input = input("Enter your password : ") + "\n"
+    print("Enter your password : ")
+    password_input = input("-> ") + "\n"
 
     if password_input == password:
         print("Correct password, guaranteed access.")
     else:
         while password_input != password:
-            password_input = input("Wrong password, please try again : ") + "\n"
+            print("Wrong password, please try again : ")
+            password_input = input("-> ") + "\n"
         print("Correct password, guaranteed access.")
 
     userdata[1] = password_input
+
+    text_system(username_data, username_idx)
 
 
 def register(un, ulines, plines, data_):
@@ -42,15 +47,19 @@ def register(un, ulines, plines, data_):
             twin += 1
 
     if twin > 0:
-        username_ = input("This username already exist, please try again : ")
+        print("This username already exist, please try again : ")
+        username_ = input("-> ")
         while username_ == "":
-            username_ = input("Invalid username, please try again : ")
+            print("Invalid username, please try again : ")
+            username_ = input("-> ")
         register(username_.lower(), ulines, plines, data_)
     else:
         print("---------Register---------")
-        password = input("Enter a new password : ")
+        print("Enter a new password : ")
+        password = input("-> ")
         while len(password) < 4:
-            password = input("Your password needs to be at least 4 character long ! Please try again :")
+            print("Your password needs to be at least 4 character long ! Please try again :")
+            password = input("-> ")
 
         with open('usernames', 'a') as u:
             u.write(un + "\n")
@@ -59,9 +68,51 @@ def register(un, ulines, plines, data_):
         print("Username + password successfully added to username.txt.")
 
 
-def text_system():
-    # to do
-    pass
+def text_system(ud, i):
+    i -= 1
+    print("-----" + userdata[0] + "-----")
+    print("--- What do you want to do :")
+    print("--- Modify your line plot - m")
+    print("--- Read someone's line plot - r")
+
+    command_ = input("-> ")
+
+    while command_ != "m" and command_ != "r":
+        print("Invalid command, please try again : ")
+        command_ = input("-> ")
+
+    if command_ == "m":
+        try:
+            with open("text", "r") as file:
+                data = file.readlines()
+                print("Enter your text here : ")
+                data[i] = input("-> ")
+            with open("text", "w") as file:
+                file.writelines(data)
+        except:
+            print(i)
+
+    else:
+        print("The line of text from whom do you want to read ?")
+        user = input("-> ")
+        username_error = True
+        while username_error:
+            username_idx = 0
+            for l in ud:
+                line = ""
+                for i in range(len(l) - 1):
+                    line += l[i]
+                if user == line:
+                    username_error = False
+                username_idx += 1
+
+            if username_error:
+                print("Error, this username doesn't exist, please try again : ")
+                user = input("-> ")
+
+        with open("text", "r") as file:
+            data = file.readlines()
+            print(data[username_idx - 1])
 
 
 def create_files():
@@ -83,7 +134,7 @@ def create_files():
     except:
         open("text", "w")
         print("File 'text' doesn't exist, new file created.")
-    
+
 
 def engine():
     create_files()
@@ -91,9 +142,11 @@ def engine():
     f = open("usernames")
     usernames_data = f.readlines()
     f.close()
-
     f = open("passwords")
     passwords_data = f.readlines()
+    f.close()
+    f = open("text")
+    t = f.readlines()
     f.close()
 
     with open(r"usernames", 'r') as fp:
@@ -101,25 +154,29 @@ def engine():
     with open(r"passwords", 'r') as fp:
         plines = len(fp.readlines())
 
-    print("Type a command : \n--- l = login (connect to an account)\n--- r = register (create a new account)")
-    command = input("\n")
+    print("Type a command : \n--- l = login (connect to an account)\n--- r = register (create a new account)\n")
+    command = input("-> ")
     while command != "l" and command != "r":
         print("Invalid command, please try again : ")
-        command = input("\n")
+        print("\n")
+        command = input("-> ")
 
     if command == "l":
         print("---------Login---------")
-        username = input("Enter your username : ")
-        login(usernames_data, passwords_data, username)
+        print("Enter your username : ")
+        username = input("-> ")
+        login(usernames_data, passwords_data, username, t)
     else:
         print("---------Register---------")
-        username = input("Create your new username : ")
+        print("Create your new username : ")
+        username = input("-> ")
         while username == "":
-            username = input("Invalid username, please try again : ")
+            print("Invalid username, please try again : ")
+            username = input("-> ")
         register(username.lower(), ulines, plines, usernames_data)
         engine()
 
-    text_system()
+
 
 
 engine()
